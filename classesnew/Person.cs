7f62388014c2bfd6577b4d68e905cf8980aef117
lonsdale228 <gerashchenkovladislav@onua.edu.ua>
2022-01-01 19:21:50 +0200

    Добавьте файлы проекта.

---
+++ classesnew/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace classesnew
+{
+    public class Person
+    {
+        public string SecondName { get; set; }
+        public string Name { get; private set; }
+
+        public Person(string secondName, string name)
+        {
+
+            SecondName = secondName;
+            Name = name;
+        }
+            
+
+
+        //public string Fullname
+        //{
+        //    get
+        //    {
+        //        return SecondName + " " + Name;
+        //    }
+        //}
+
+        //public string ShortName
+        //{
+        //    get
+        //    {
+        //        return $"{SecondName} {Name.Substring(0, 1)}.";
+        //    }
+        //}
+    }
+}
---
+++ classesnew/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace classesnew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+
+            Person p = new Person("Пупкин", "Вася");
+            //p.Name = "Вася";
+            //p.SecondName = "Пупкин";
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.SecondName);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
